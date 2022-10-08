4071fc2007df15fee607acace9871888e48234b5
qJay44 <m70155302@yandex.ru>
2022-10-08 13:13:07 +0300

    test commit

--- flask_app.py
+++ flask_app.py
@@ -6,7 +6,7 @@ app = Flask(__name__)
 @app.route('/update_server', methods=['POST'])
 def webhook():
     if request.method == 'POST':
-        repo = git.Repo('path/to/git_repo')
+        repo = git.Repo('https://github.com/qJay44/Univ-API')
         origin = repo.remotes.origin
 
         origin.pull()

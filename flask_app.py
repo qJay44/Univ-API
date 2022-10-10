43313f1607c8ee4f36fd6f4c13012cf1d5ea2014
qJay44 <m70155302@yandex.ru>
2022-10-10 21:17:30 +0300

    another hook test

--- flask_app.py
+++ flask_app.py
@@ -1,5 +1,8 @@
 from flask import Flask, request
+import requests
+import json
 import git
+import os
 
 app = Flask(__name__)
 
@@ -15,4 +18,50 @@ def webhook():
     else:
         return 'Wrong event type', 400
 
-'test string'
+
+@app.route('/api/v1/getImg/<file_name>', method=['GET'])
+def getImg(file_name):
+    for root, dirs, files in os.walk('/home/qJay44/mysite/files/'):
+        if file_name in files:
+            return os.path.join(root, file_name)
+
+    # convert(file_name)
+
+
+def convert(file_name):
+
+    instructions = {
+      'parts': [
+        {
+          'file': 'document'
+        }
+      ],
+      'output': {
+        'type': 'image',
+        'format': 'png',
+        'dpi': 500
+      }
+    }
+
+    response = requests.request(
+      'POST',
+      'https://api.pspdfkit.com/build',
+      headers = {
+        'Authorization': 'Bearer pdf_live_qpcg2HFUbaP4u52RoRabxOb8DFxL5GMoxQyh4xGGMsq'
+      },
+      files = {
+        'document': open(f'{file_name}.xls', 'rb')
+      },
+      data = {
+        'instructions': json.dumps(instructions)
+      },
+      stream = True
+    )
+
+    if response.ok:
+      with open('image.png', 'wb') as fd:
+        for chunk in response.iter_content(chunk_size=8096):
+          fd.write(chunk)
+    else:
+      print(response.text)
+

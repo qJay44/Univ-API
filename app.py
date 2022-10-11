3930c634b708d9d1000ee92f22b05210f5e70095
qJay44 <m70155302@yandex.ru>
2022-10-12 01:19:43 +0300

    web scrapping added

--- app.py
+++ app.py
@@ -1,13 +1,21 @@
 from flask import Flask, request
 from bs4 import BeautifulSoup
+from selenium import webdriver
+from werkzeug.exceptions import RequestTimeout
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.common.by import By
+from selenium.common.exceptions import TimeoutException
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 import requests as rq
 import json
 import git
 import os
-import sys
 import logging
+import uuid
 
 app = Flask(__name__)
+app.config['TRAP_HTTP_EXCEPTIONS'] = True
 logging.basicConfig(filename="info.log", level=logging.INFO)
 
 
@@ -26,26 +34,9 @@ def webhook():
 
 @app.route('/api/v1/check_update')
 def check_update():
-    items_dict = {}
-    url = 'https://www.muiv.ru/studentu/fakultety-i-kafedry/fakultet-it/raspisaniya/'
-    soup = BeautifulSoup(rq.get(url).text, 'lxml')
-    files_div = soup.find('div', class_='m-intext-docs')
-    logging.info(files_div)
-
-    for i, item in enumerate(files_div.find_all('div', class_='m-doc')):
-        for n, par in enumerate(item.find_all('p')):
-            logging.info(par)
-        url = item.p.a['href']
-        url_items = url.split('/')
-        items_dict[f'doc{i}'] = {
-            'url': f'https://www.muiv.ru{url}',
-            'id': url_items[3],
-            'fileName': url_items[4],
-            'fileSize': item.p.text
-        }
-
-    return items_dict
-
+    webData = getWebData()
+    dbData = ...
+    return webData
 
 
 @app.route('/api/v1/get/img/<string:file_name>', methods=['GET'])
@@ -56,6 +47,72 @@ def getImg(file_name):
     return 'None'
 
     # convert(file_name)
+
+
+@app.errorhandler(Exception)
+def hadle_error(e):
+    if e.code == 504:
+        return 'Page load timeout'
+
+
+
+def getWebData():
+    """
+    FIXME: Page loading problem
+
+    forms[0] = Очная
+    forms[1] = Заочная
+    forms[2] = Очно-заочная
+
+    """
+    forms = [
+        '\u041e\u0447\u043d\u0430\u044f',
+        '\u0417\u0430\u043e\u0447\u043d\u0430\u044f',
+        '\u041e\u0447\u043d\u043e-\u0437\u0430\u043e\u0447\u043d\u0430\u044f'
+    ]
+    url = 'https://www.muiv.ru/studentu/fakultety-i-kafedry/fakultet-it/raspisaniya/'
+
+    source = tryToLoadPage(url)
+    soup = BeautifulSoup(source, 'lxml')
+    files_div = soup.find('div', class_='m-intext-docs')
+    items_dict = {}
+
+    for item in files_div.find_all('div', class_='m-doc'):
+        file_props = [p for p in item.find_all('p')]
+        url = file_props[0].a['href']
+        url_items = url.split('/')
+        original_name = file_props[0].a.text
+        eduForm = [f for f in forms if f in original_name][0]
+        items_dict[str(uuid.uuid4())] = {
+            'eduForm': eduForm,
+            'url': f'https://www.muiv.ru{url}',
+            'id': url_items[3],
+            'fileName': url_items[4],
+            'fileSize': file_props[1].text.split(' ')[1],
+            'updateTime': file_props[2].text[-10:],
+            'origName': original_name,
+        }
+
+    return items_dict
+
+
+def tryToLoadPage(url):
+    url_request = rq.get(url).text
+    if 'https://static.stormwall.pro/ajax-loader.gif' in url_request:
+        try:
+            options = Options()
+            options.headless = True
+            driver = webdriver.Chrome(options=options)
+            driver.get(url)
+            wait = WebDriverWait(driver, 10)
+            wait.until(EC.presence_of_element_located((By.CLASS_NAME, 'm-intext-docs')))
+        except TimeoutException:
+            logging.error('Page loading timeout exception')
+            raise RequestTimeout()
+        finally:
+            return driver.page_source
+
+    return url_request
 
 
 def convert(file_name):

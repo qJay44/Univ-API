088240c3fabcc77aae07be8c3137c9b123ca889b
qJay44 <m70155302@yandex.ru>
2022-10-11 19:15:33 +0300

    site parser

--- app.py
+++ app.py
@@ -1,15 +1,20 @@
 from flask import Flask, request
-import requests
+from bs4 import BeautifulSoup
+import requests as rq
 import json
 import git
 import os
+import sys
+import logging
 
 app = Flask(__name__)
+logging.basicConfig(filename="info.log", level=logging.INFO)
+
 
 @app.route('/update_server', methods=['POST'])
 def webhook():
     if request.method == 'POST':
-        repo = git.Repo('./Univ-API')
+        repo = git.Repo('./UnivAPI')
         origin = repo.remotes.origin
 
         origin.pull()
@@ -19,18 +24,38 @@ def webhook():
         return 'Wrong event type', 400
 
 
-@app.route('/api/v1/getImg/<file_name>', methods=['GET'])
+@app.route('/api/v1/check_update')
+def check_update():
+    items_dict = {}
+    url = 'https://www.muiv.ru/studentu/fakultety-i-kafedry/fakultet-it/raspisaniya/'
+    soup = BeautifulSoup(rq.get(url).text, 'lxml')
+    files_div = soup.find('div', class_='m-intext-docs')
+    logging.info(files_div)
+
+    for i, item in enumerate(files_div.find_all('div', class_='m-doc')):
+        for n, par in enumerate(item.find_all('p')):
+            logging.info(par)
+        url = item.p.a['href']
+        url_items = url.split('/')
+        items_dict[f'doc{i}'] = {
+            'url': f'https://www.muiv.ru{url}',
+            'id': url_items[3],
+            'fileName': url_items[4],
+            'fileSize': item.p.text
+        }
+
+    return items_dict
+
+
+
+@app.route('/api/v1/get/img/<string:file_name>', methods=['GET'])
 def getImg(file_name):
-    for root, dirs, files in os.walk('/home/qJay44/mysite/files/'):
+    for root, _, files in os.walk('./UnivAPI/files/'):
         if file_name in files:
             return os.path.join(root, file_name)
+    return 'None'
 
     # convert(file_name)
-
-
-@app.route('/test', methods=['GET'])
-def test():
-    return 'test'
 
 
 def convert(file_name):
@@ -48,7 +73,7 @@ def convert(file_name):
       }
     }
 
-    response = requests.request(
+    response = rq.request(
       'POST',
       'https://api.pspdfkit.com/build',
       headers = {
